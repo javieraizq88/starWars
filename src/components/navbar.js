e4aaf29940e7c5423f68b80038b1a6b3fe110033
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-05-01 11:55:18 -0400

    actualizado el navbar y bg films"

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,28 +5,29 @@ const Navbar = props => {
     return (
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-light " id="navbar">
+            <a class="navbar-brand" href="#">Star Wars</a>
                 <div className="collapse navbar-collapse " id="navbarNav">
-                    <ul className="navbar-nav" id="navbar-item">
-                        <li className="nav-item" >
-                            <Link className="nav-link" to="/">Home</Link>
+                    <ul className="navbar-nav mr-auto border-right" id="navbar-item">
+                        <li className="nav-item active " >
+                            <Link className="nav-link border-left" to="/">Home<span class="sr-only">(current)</span></Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/people">People</Link>
+                            <Link className="nav-link border-left" to="/people">People</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/films">Films</Link>
+                            <Link className="nav-link border-left" to="/films">Films</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/starships">Starships</Link>
+                            <Link className="nav-link border-left" to="/starships">Starships</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/vehicles">Vehicles</Link>
+                            <Link className="nav-link border-left" to="/vehicles">Vehicles</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/species">Species</Link>
+                            <Link className="nav-link border-left" to="/species">Species</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/planets">Planets</Link>
+                            <Link className="nav-link border-left" to="/planets">Planets</Link>
                         </li>
                     </ul>
                 </div>
